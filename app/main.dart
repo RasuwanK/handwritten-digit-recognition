24e5a616cf78912d25dae237182977fafd977a54
shenal <riyoshenal@gmail.com>
2026-04-25 14:52:13 +0530

    mobile app added

---
+++ app/main.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  File? _image;
+  String result = "";
+
+  final picker = ImagePicker();
+
+  Future pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+
+      uploadImage(File(pickedFile.path));
+    }
+  }
+
+  Future uploadImage(File imageFile) async {
+    var request = http.MultipartRequest(
+      'POST',
+      Uri.parse('http://YOUR_IP:5000/predict'),
+    );
+
+    request.files.add(
+      await http.MultipartFile.fromPath('image', imageFile.path),
+    );
+
+    var response = await request.send();
+    var res = await http.Response.fromStream(response);
+
+    var data = json.decode(res.body);
+
+    setState(() {
+      result = "Predicted Digit: ${data['digit']}";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Digit Recognizer")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _image != null
+              ? Image.file(_image!, height: 200)
+              : Text("No image selected"),
+          SizedBox(height: 20),
+          Text(result, style: TextStyle(fontSize: 24)),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: pickImage,
+            child: Text("Capture Digit"),
+          ),
+        ],
+      ),
+    );
+  }
+}
